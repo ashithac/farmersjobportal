8b7cbde0b33db00d828c66419adcec5bf380a08d
ashithac <ashithacaachii@gmail.com>
2021-07-07 16:18:09 +0530

    dev latest

---
+++ config/collections.js
@@ -0,0 +1,4 @@
+module.exports={
+    JOBS_COLLECTION:'jobs',
+    USER_COLLECTION:'user'
+}
---
+++ helpers/job-helpers.js
@@ -0,0 +1,17 @@
+var db=require('../config/connection')
+var collection=require('../config/collections')
+module.exports={
+    addJob:(jobs,callback)=>{
+        //console.log(jobs)
+        db.get().collection('jobs').insertOne(jobs).then((data)=>{
+            console.log(data)
+            callback(data.ops[0]._id)
+        })
+    },
+    getAllJobs:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let jobs=await db.get().collection(collection.JOBS_COLLECTION).find().toArray()
+            resolve(jobs)
+        })
+    }
+}
